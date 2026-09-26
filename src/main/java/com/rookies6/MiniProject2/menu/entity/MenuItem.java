05b430446005eff66cb20eb7d84d9a7a8935c0f9
creatorsg <soninseon2004@gmail.com>
2026-09-26 15:11:23 +0900

    MenuItem ì초기 완성 & Store임시 클래스 작성

--- src/main/java/com/rookies6/MiniProject2/menu/entity/MenuItem.java
+++ src/main/java/com/rookies6/MiniProject2/menu/entity/MenuItem.java
@@ -1,4 +1,35 @@
 package com.rookies6.MiniProject2.menu.entity;
 
-public class MenuItem {
+import com.rookies6.MiniProject2.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "menu_items")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter @Builder
+public class MenuItem extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(name = "is_sold_out", nullable = false)
+    private boolean soldOut;
+
+    @Column(name = "image_url")
+    private String image_url;
+
+    public void toggleSoldOut() {
+        this.soldOut = !this.soldOut;
+    }
 }
